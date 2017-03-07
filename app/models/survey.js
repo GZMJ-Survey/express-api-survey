22f781cf4d6523bdd1d6f62e90ab5feb813c0a49
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-03-07 16:00:15 -0500

    Implement Survey Model
    Duplicated from examples model. Added in question and answer
    attributes and removed text attribute. Replaced all example keywords
    with survey.

---
+++ app/models/survey.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const surveySchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true,
+  },
+  answer: {
+    type: Boolean,
+    required: false,
+  },
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: function (doc, ret, options) {
+      let userId = (options.user && options.user._id) || false;
+      ret.editable = userId && userId.equals(doc._owner);
+      return ret;
+    },
+  },
+});
+
+surveySchema.virtual('length').get(function length() {
+  return this.text.length;
+});
+
+const Survey = mongoose.model('Survey', surveySchema);
+
+module.exports = Survey;
