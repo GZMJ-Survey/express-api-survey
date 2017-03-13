c1959d7bf55fc8cc65e56a7fdcc6810fdb73bac9
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-03-13 14:35:08 -0400

    Log for debugging

--- app/controllers/surveys.js
+++ app/controllers/surveys.js
@@ -70,11 +70,11 @@ const update = (req, res, next) => {
           // console.log('survey.questions[i].answers[newAnswers]', survey.questions[i].answers[newAnswers]);
           // console.log('if:', survey.questions[i].answers[newAnswers]===undefined);
           // console.log('req.body.survey.questions[i].answers', req.body.survey.questions[i].answers);
-          if (survey.questions[i].answers[newAnswers]===undefined) {
-             console.log(survey.questions[i].answers.push(req.body.survey.questions[i].answers));
-          } else {
-          survey.questions[i].answers[newAnswers] = survey.questions[i].answers[newAnswers] || req.body.survey.questions[i].answers;
-          }
+          // if (survey.questions[i].answers[newAnswers]===undefined) {
+             console.log(survey.questions[i].answers[newAnswers] = req.body.survey.questions[i].answers);
+          // } else {
+          // survey.questions[i].answers[newAnswers] = survey.questions[i].answers[newAnswers] || req.body.survey.questions[i].answers;
+          // }
         }
 
       }
